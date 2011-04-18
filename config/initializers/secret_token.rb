a5f7b9716006ff46e8d2f559172635359a841694
Alex Killough <alex@firebellydesign.com>
2011-04-18 11:14:43 -0500

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bazaar::Application.config.secret_token = '0af9b4270fa22889c6bea5d222a2e22ed42421f87d86681b5eb00704359158c015ed910b162b651a6d7405b7c2a81c029292fcd92b7a230171f56fd79ed47a6e'
